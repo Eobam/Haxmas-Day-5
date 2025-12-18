bbabf58dd49fe261d65392faa330b2e0ca49101a
Eobam <117958786+Eobam@users.noreply.github.com>
2025-12-18 06:40:14 -0500

    Simplify app.run() by using default settings
    Removed port configuration from app.run()

--- main.py
+++ main.py
@@ -80,5 +80,4 @@ def get_stats():
     return flask.jsonify(stats)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run()
